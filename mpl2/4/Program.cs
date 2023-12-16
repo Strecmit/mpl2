de615ef57fc19addba194db02f411790901c21ae
Victor <strecmit@gmail.com>
2023-12-16 18:26:44 +0200

    четвёртая задача

---
+++ mpl2/4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num_l = num;
+int n = 0;
+while ((num_l / 10)!=0){
+    num_l = num_l /10;
+    n++;
+}
+//Console.WriteLine(i);
+int[] arr = new int[n+1];
+int i =0, m=n;
+
+while(i <= n){
+    arr[m] = ((num+10) % 10);
+    num = (num / 10);
+    i++;m--;
+}
+foreach(int e in arr){
+    Console.Write($"{e} ");
+
+}
