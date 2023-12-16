29057ae09860b0828523e9bd39bb93ba72f88eed
Victor <strecmit@gmail.com>
2023-12-16 15:47:04 +0200

    задача 1

---
+++ mpl2/1/Program.cs
@@ -0,0 +1,8 @@
+﻿int number =0;
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+    {
+        Console.WriteLine($"Введенное число {number} кратно 7 и 23.");
+    }
+else {Console.WriteLine($"Введенное число {number} не кратно 7 и 23.");}
