e5a02973b26aa94c21799bccab76ba4777352c60
Victor <strecmit@gmail.com>
2023-12-16 16:28:11 +0200

    третья задача

---
+++ mpl2/3/Program.cs
@@ -0,0 +1,9 @@
+﻿int num = 0, max = 0;
+Console.Write("Введите двузначное число: ");
+num = Convert.ToInt32(Console.ReadLine());
+max = num / 10;
+if (num > 9 && num <= 99){
+    if ((num % 10) > max){max = num % 10;}
+    Console.WriteLine($"Большая цифра в ведённом числе {max}.");
+}
+else {Console.WriteLine($"Введённое число {num} не двузначное");}
