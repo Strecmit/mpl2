e530b3bbe6bdd662f3a93dc4b214305eaf57b64a
Victor <strecmit@gmail.com>
2023-12-16 16:04:58 +0200

    вторая задача

---
+++ mpl2/2/Program.cs
@@ -0,0 +1,23 @@
+﻿int num_x = 0, num_y = 0;
+Console.Write("Введите координату Х: ");
+num_x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату Y: ");
+num_y = Convert.ToInt32(Console.ReadLine());
+
+if(num_x!=0 && num_y!=0)
+{
+    if(num_x >0 && num_y>0){
+        Console.WriteLine($"Точка с координатами ({num_x},{num_y}) относится к I четверти плоскости");
+    }
+    if(num_x <0 && num_y>0){
+        Console.WriteLine($"Точка с координатами ({num_x},{num_y}) относится ко II четверти плоскости");
+    }
+    if(num_x <0 && num_y<0){
+        Console.WriteLine($"Точка с координатами ({num_x},{num_y}) относится к III четверти плоскости");
+    }
+    if(num_x >0 && num_y<0){
+        Console.WriteLine($"Точка с координатами ({num_x},{num_y}) относится к IV четверти плоскости");
+    }
+}
+else {Console.WriteLine($"Координата ({num_x},{num_y} задана неверно)");}
